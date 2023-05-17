4c6fc771c8c2e01a6a3b366e16a43b03c8af6a93
caiquesouza10 <caique_souza2@hotmail.com>
2023-05-17 20:27:28 -0300

    Criação da Primeira rota

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log(" Growdev");
+import express, { Request, Response } from "express";
+import { Growdev } from "./models/growdever";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  console.log("Entrou na rota");
+
+  const growdev1 = new Growdev("1", "caique", 29);
+
+  res.status(200).send({ ok: true, data: growdev1.toJson() });
+});
+
+app.listen(3333, () => {
+  console.log(`API Está rodando`);
+});
